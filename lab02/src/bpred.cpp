a1afc374e04997fec0040b8f531b0794fd6af622
jack-mil <62065280+jack-mil@users.noreply.github.com>
2023-09-17 19:03:44 -0400

    [Lab02] Fix PHT state update and add gselect index method

--- lab02/src/bpred.cpp
+++ lab02/src/bpred.cpp
@@ -1,47 +1,58 @@
 #include "bpred.h"
-#include <iostream>
+#include <algorithm>
 
 #define TAKEN true
 #define NOTTAKEN false
 
-const uint32_t mask_14 = (1 << 14) - 1;
+const uint32_t MASK14 = (1 << 14) - 1;
+const uint32_t MASK7 = (1 << 7) - 1;
 
 // I would have made these methods part of the BPRED class,
 // But I can't modify the "bpred.h" file.
 
 bool gshare_predict(BPRED *pred, uint32_t PC) {
 	// Index into PHT is 14 bits of PC XORed with the 14 bits of GHR
-	uint16_t index = (PC ^ pred->global_hist_reg) & mask_14;
+	uint16_t index = (PC ^ pred->global_hist_reg) & MASK14;
 	// Predict TAKEN if counter is '11' or '10', NOT-TAKEN if '01' or '00'
 	return pred->gshare_pattern_table[index] >= 2 ? TAKEN : NOTTAKEN;
 }
 
-void gshare_update(BPRED *pred, uint32_t PC, bool resolveDir) {
-	// Index into PHT is 14 bits of PC XORed with the 14 bits of GHR
-	uint16_t index = (PC ^ pred->global_hist_reg) & mask_14;
+void update_at_index(BPRED *pred, uint16_t index, bool resolveDir) {
 	uint32_t curr_state = pred->gshare_pattern_table[index];
 	uint32_t next_state;
 	uint32_t next_ghr;
 	if ( resolveDir == TAKEN ) {
 		// Increment the state counter up to 3 (modulo 4) on a taken branch
-		next_state = (curr_state + 1) % 4;
+		next_state = curr_state < 4 ? curr_state + 1 : 3;
 		// Shift a 1 into GHR, then mask to 14 bits
-		next_ghr = ((pred->global_hist_reg << 1) + 1) & mask_14;
+		next_ghr = ((pred->global_hist_reg << 1) + 1u) & MASK14;
 	} else {
 		// Decrement the state counter down to 0 on a not-taken branch
 		next_state = curr_state > 0 ? curr_state - 1 : 0;
 		// Shift a 0 into GHR, then mask to 14 bits
-		next_ghr = (pred->global_hist_reg << 1) & mask_14;
+		next_ghr = (pred->global_hist_reg << 1) & MASK14;
 	}
 	// Update the Pattern History Table
 	pred->gshare_pattern_table[index] = next_state;
 	// Update the Global History Register
 	pred->global_hist_reg = next_ghr;
 }
+void gshare_update(BPRED *pred, uint32_t PC, bool resolveDir) {
+	// Index into PHT is 14 bits of PC XORed with the 14 bits of GHR
+	uint16_t index = (PC ^ pred->global_hist_reg) & MASK14;
+	update_at_index(pred, index, resolveDir);
+}
+
 bool gselect_predict(BPRED *pred, uint32_t PC) {
-	return TAKEN;
+	// Index into PHT is 7 low bits of PC concat with the 7 low bits of GHR
+	uint16_t index = ((PC & MASK7) << 7) | (pred->global_hist_reg & MASK7);
+	// Predict TAKEN if counter is '11' or '10', NOT-TAKEN if '01' or '00'
+	return pred->gshare_pattern_table[index] >= 2 ? TAKEN : NOTTAKEN;
 }
 void gselect_update(BPRED *pred, uint32_t PC, bool resolveDir) {
+	// Index into PHT is 7 low bits of PC concat with the 7 low bits of GHR
+	uint16_t index = ((PC & MASK7) << 7) | (pred->global_hist_reg & MASK7);
+	update_at_index(pred, index, resolveDir);
 }
 bool tournament_predict(BPRED *pred, uint32_t PC) {
 	return TAKEN;
@@ -60,7 +71,17 @@ BPRED::BPRED(uint32_t policy) {
 	// Set instance policy
 	this->policy = static_cast<BPRED_TYPE>(policy);
 
-	//std::cout << "Predictor Initilized with Policy [" << this->policy << "]" << std::endl;
+	this->stat_num_branches = 0;
+	this->stat_num_mispred = 0;
+
+	this->global_hist_reg = 0;
+
+	// Initilize the PHT to the weakly taken state "10" = 2;
+	std::fill_n(gshare_pattern_table, 1 << 14, 2u);
+	std::fill_n(this->gselect_pattern_table, 1 << 14, 2u);
+	std::fill_n(this->tournament_pattern_table, 1 << 10, 2u);
+
+	// std::cout << "Predictor Initilized with Policy [" << this->policy << "]" << std::endl;
 }
 
 /////////////////////////////////////////////////////////////

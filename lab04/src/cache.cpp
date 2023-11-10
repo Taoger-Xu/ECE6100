9fda9f24a0b80883388042c86d0223665152d77e
jack-mil <62065280+jack-mil@users.noreply.github.com>
2023-11-10 14:22:10 -0500

    [Lab04] Remove cache_set struct, fix some cache_line initilization issue, break(?) dirty tracking more

--- lab04/src/cache.cpp
+++ lab04/src/cache.cpp
@@ -14,24 +14,29 @@ extern uint64_t cycle;
 /////////////////////////////////////////////////////////////////////////////////////
 
 void cache_print_stats(Cache *c, char *header) {
+	c->print_stats(header);
+}
+
+void Cache::print_stats(const char *header) {
 	double read_mr = 0;
 	double write_mr = 0;
 
-	if ( c->stat_read_access ) {
-		read_mr = (double)(c->stat_read_miss) / (double)(c->stat_read_access);
+	if ( this->stat_read_access ) {
+		read_mr = (double)(this->stat_read_miss) / (double)(this->stat_read_access);
 	}
 
-	if ( c->stat_write_access ) {
-		write_mr = (double)(c->stat_write_miss) / (double)(c->stat_write_access);
+	if ( this->stat_write_access ) {
+		write_mr = (double)(this->stat_write_miss) / (double)(this->stat_write_access);
 	}
 
-	printf("\n%s_READ_ACCESS    \t\t : %10lu", header, c->stat_read_access);
-	printf("\n%s_WRITE_ACCESS   \t\t : %10lu", header, c->stat_write_access);
-	printf("\n%s_READ_MISS      \t\t : %10lu", header, c->stat_read_miss);
-	printf("\n%s_WRITE_MISS     \t\t : %10lu", header, c->stat_write_miss);
+	printf("\n%s_READ_ACCESS    \t\t : %10lu", header, this->stat_read_access);
+	printf("\n%s_WRITE_ACCESS   \t\t : %10lu", header, this->stat_write_access);
+	printf("\n%s_READ_MISS      \t\t : %10lu", header, this->stat_read_miss);
+	printf("\n%s_WRITE_MISS     \t\t : %10lu", header, this->stat_write_miss);
 	printf("\n%s_READ_MISS_PERC  \t\t : %10.3f", header, 100 * read_mr);
 	printf("\n%s_WRITE_MISS_PERC \t\t : %10.3f", header, 100 * write_mr);
-	printf("\n%s_DIRTY_EVICTS   \t\t : %10lu", header, c->stat_dirty_evicts);
+	printf("\n%s_TOTAL_EVICTS   \t\t : %10lu", header, this->stat_evicts);
+	printf("\n%s_DIRTY_EVICTS   \t\t : %10lu", header, this->stat_dirty_evicts);
 	printf("\n");
 }
 
@@ -46,8 +51,9 @@ Cache *cache_new(uint64_t size, uint64_t assoc, uint64_t line_size, uint64_t rep
 	uint64_t num_sets = (size / line_size) / assoc;
 
 	assert(assoc <= MAX_WAYS);
-
-	return new Cache(num_sets, assoc, line_size, (Repl_Policy)repl_policy);
+	Cache *cache = new Cache(num_sets, assoc, (Repl_Policy)repl_policy);
+	// Cache _cache = Cache(num_sets, assoc, (Repl_Policy)repl_policy);
+	return cache;
 }
 
 /////////////////////////////////////////////////////////////////////////////////////
@@ -67,27 +73,23 @@ bool Cache::access(Addr lineaddr, bool is_write, uint32_t core_id) {
 		stat_read_access++;
 	};
 
-	// uint64_t tag_and_index = lineaddr >> (uint64_t)std::ceil(std::log2(m_block_size));
-	// auto index_bits = (uint64_t)std::ceil(std::log2(num_sets));
-	// uint64_t tag = tag_and_index >> index_bits;
-
 	// lineaddr is already tag and index portion (blocksize extracted in memsys.cpp)
 	uint64_t tag = lineaddr / m_num_sets;               // Integer division shenanigans. Tag is probably not necessary, just store the whole lineaddr in ways
 	uint64_t index = lineaddr & (this->m_num_sets - 1); //(num_sets is power-of-2, so get the bitmask (e.g. 64 -> 100_0000 - 1 = 11_1111)
 
 	// Random access into the set for this index
-	Cache_Set &set = this->sets.at(index);
+	auto &set = this->sets.at(index);
 
 	// Sanity check
-	assert(set.ways.size() <= this->m_assoc);
+	assert(set.size() <= this->m_assoc);
 	// Search for a cache line in ways of this set
 	// Uses C++11 Lambda to equate cache lines by tag (and ensure valid)
-	auto it = std::find_if(set.ways.begin(),
-	                       set.ways.end(),
+	auto it = std::find_if(set.begin(),
+	                       set.end(),
 	                       [&](const Cache_Line &line) { return (line.valid) && (line.tag == tag); });
 
 	// The tag was not found, MISS condition
-	if ( it == set.ways.end() ) {
+	if ( it == set.end() ) {
 		// Update stats and return false
 		if ( is_write ) {
 			stat_write_miss++;
@@ -102,14 +104,15 @@ bool Cache::access(Addr lineaddr, bool is_write, uint32_t core_id) {
 	// This self organizing ensures most recently used at front
 	// and makes read/write access to this element fast
 	// (still results in a walk down the LL, but better than deallocate and re-insert)
-	set.ways.splice(set.ways.begin(), set.ways, it);
+	set.splice(set.begin(), set, it);
 
 	// Now we can access the line we just hit at front()
 	// by updating state
-	if ( set.ways.front().lfu_count < LFU_cnt_max )
-		set.ways.front().lfu_count++;
-	set.ways.front().dirty = is_write;
-	set.ways.front().last_access_cycle = cycle;
+	if ( set.front().lfu_count < LFU_cnt_max )
+		set.front().lfu_count++;
+	// Once the line becomes dirty, it remains dirty until evicted (writeback)
+	set.front().dirty = !set.front().dirty ? is_write : true;
+	set.front().last_access_cycle = cycle;
 
 	return HIT;
 }
@@ -129,17 +132,21 @@ void Cache::install(Addr lineaddr, bool is_write, uint32_t core_id) {
 	uint64_t index = lineaddr & (m_num_sets - 1); //(num_sets is power-of-2, so get the bitmask (e.g. 64 -> 100_0000 - 1 = 11_1111)
 
 	// Random access into the set for this index
-	Cache_Set &set = sets.at(index);
+	auto &set = sets.at(index);
 
-	if ( set.ways.size() >= this->m_assoc ) {
-		this->find_victim(index, core_id);		// Find victim will make space in the set
+	if ( set.size() >= this->m_assoc ) {
+		this->find_victim(index, core_id); // Find victim will make space in the set
 	}
-	
+
 	// Prep the new line
-	Cache_Line new_line = Cache_Line(tag, is_write, core_id, cycle);
-	new_line.valid = true;		// Ensure it's valid (default true in constructor)
+	const Cache_Line &new_line = {.tag = tag,
+	                              .valid = true,
+	                              .dirty = is_write,
+	                              .core_id = core_id,
+	                              .last_access_cycle = cycle,
+	                              .lfu_count = 0};
 	// Insert it into the most recently accessed spot
-	set.ways.push_front(new_line);
+	set.push_front(new_line);
 
 	return;
 }
@@ -148,12 +155,12 @@ void Cache::install(Addr lineaddr, bool is_write, uint32_t core_id) {
 uint32_t Cache::find_victim(uint32_t set_index, uint32_t core_id) {
 
 	Cache_Line victim;
-	Cache_Set &set = this->sets.at(set_index);
+	auto &set = this->sets.at(set_index);
 	switch ( this->m_repl_policy ) {
 		case LRU:
 			// Save the last element in the DLL and remove it
-			victim = set.ways.back();
-			set.ways.pop_back();
+			victim = set.back();
+			set.pop_back();
 			break;
 		case LFU_MRU:
 			break;
@@ -162,7 +169,8 @@ uint32_t Cache::find_victim(uint32_t set_index, uint32_t core_id) {
 		default:
 			assert(false); // uh oh
 			break;
-	}
+	};
+	this->stat_evicts++;
 
 	// Track evictions
 	if ( victim.dirty )
--- lab04/src/cache.h
+++ lab04/src/cache.h
@@ -22,7 +22,7 @@ enum Repl_Policy {
 
 // A cache line holds information about the memory block it holds
 struct Cache_Line {
-	uint32_t tag; // Higher order address bits beyond index. Identifies the data
+	uint64_t tag; // Higher order address bits beyond index. Identifies the data
 	bool valid;   // If line is present in the cache
 	bool dirty;   // If data has been written to (present only in the cache)
 
@@ -30,51 +30,43 @@ struct Cache_Line {
 
 	uint64_t last_access_cycle; // Track for LRU/MRU replacement policy (cycle)
 	uint64_t lfu_count;         // Track the number of accesses for LFU replacement policy (frequency)
-
-	Cache_Line(uint32_t tag,  bool d, uint32_t cid, uint64_t lac, bool v = true)
-	    : tag(tag), valid(v), dirty(d), core_id(cid), last_access_cycle(lac) {};
-	Cache_Line(){};
 };
 
 // A cache set it made up of 1 or more ways
 struct Cache_Set {
 	// Ways implemented as linked list for quick insertion
 	std::list<Cache_Line> ways;
-
-	// Constructor to initialize list
-	// Cache_Set() : ways(0) {
-	// };
 };
 
 // A cache with configurable number of sets and associativity (ways per set)
 class Cache {
   public:
-	uint64_t stat_read_access;  // Number of read (lookup accesses do not count as READ accesses) accesses made to the cache
-	uint64_t stat_write_access; // Number of write accesses made to the cache
-	uint64_t stat_read_miss;    // Number of READ requests that lead to a MISS at the respective cache.
-	uint64_t stat_write_miss;   // Number of WRITE requests that lead to a MISS at the respective cache
-	uint64_t stat_dirty_evicts; // Count of requests to evict DIRTY lines.
+	// Constructor for cache. Creates vector of sets with the right number of ways
+	Cache(uint32_t num_sets, uint16_t assoc, Repl_Policy policy)
+	    : m_assoc(assoc),
+	      m_num_sets(num_sets),
+	      m_repl_policy(policy),
+	      sets(num_sets){};
+	bool access(Addr lineaddr, bool is_write, uint32_t core_id);
+	void install(Addr lineaddr, bool is_write, uint32_t core_id);
+	uint32_t find_victim(uint32_t set_index, uint32_t core_id);
+	void print_stats(const char *header);
 
-	uint64_t m_block_size;
+  private:
+	uint64_t stat_read_access = 0;  // Number of read (lookup accesses do not count as READ accesses) accesses made to the cache
+	uint64_t stat_write_access = 0; // Number of write accesses made to the cache
+	uint64_t stat_read_miss = 0;    // Number of READ requests that lead to a MISS at the respective cache.
+	uint64_t stat_write_miss = 0;   // Number of WRITE requests that lead to a MISS at the respective cache
+	uint64_t stat_evicts = 0;       // Count of requests to evict DIRTY lines.
+	uint64_t stat_dirty_evicts = 0; // Count of requests to evict DIRTY lines.
+
 	uint16_t m_assoc;
 	uint16_t m_num_sets;
 	Repl_Policy m_repl_policy;
 
-	Cache_Line last_evicted;
+	Cache_Line last_evicted = {};
 
-	std::vector<Cache_Set> sets;
-
-	// Constructor for cache. Creates vector of sets with the right number of ways
-	Cache(uint32_t num_sets, uint16_t assoc, uint64_t block_size, Repl_Policy policy)
-	    : m_block_size(block_size),
-	      m_assoc(assoc),
-	      m_num_sets(num_sets),
-	      m_repl_policy(policy),
-	      sets(num_sets, Cache_Set{}){};
-
-	bool access(Addr lineaddr, bool is_write, uint32_t core_id);
-	void install(Addr lineaddr, bool is_write, uint32_t core_id);
-	uint32_t find_victim(uint32_t set_index, uint32_t core_id);
+	std::vector<std::list<Cache_Line>> sets;
 };
 
 //////////////////////////////////////////////////////////////////////////////////////

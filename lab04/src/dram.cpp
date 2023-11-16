1275694d6aeb57646ccc3360505458d3f359efa2
jack-mil <62065280+jack-mil@users.noreply.github.com>
2023-11-16 18:42:51 -0500

    [Lab04] Fix DRAM addressing, but still wrong avg delay

--- lab04/src/dram.cpp
+++ lab04/src/dram.cpp
@@ -30,7 +30,6 @@ void dram_print_stats(Dram *dram) {
 	printf("\n%s_WRITE_DELAY_AVG\t\t : %10.3f", header, wrdelay_avg);
 }
 
-
 Dram *dram_new(uint8_t policy) {
 	Dram *dram = new Dram((Page_Policy)policy);
 	return dram;
@@ -65,26 +64,29 @@ uint64_t dram_access(Dram *dram, Addr lineaddr, bool is_dram_write) {
 	return delay;
 }
 
-///////////////////////////////////////////////////////////////////
-// Modify the function below only for Parts C,D,E
-///////////////////////////////////////////////////////////////////
 
+/* Parts C,D,E Simulate DRAM open/closed page policies */
 uint64_t dram_access_mode_CDE(Dram *dram, Addr lineaddr, bool is_dram_write) {
 
 	// Short circuit all of this in case of closed page policy (no need to simulate)
 	if ( dram->page_policy == CLOSED_PAGE ) {
+		dram->stat_buffer_miss++;                       // Every access is a buffer miss if closed page policy
 		return LATENCY_BUS + LATENCY_CAS + LATENCY_RAS; // 10 + 45 + 45 = 100
 	}
 
-	// We need to simulate row buffer hit/miss for open page policy
+	// For OPEN_PAGE policy //
 
-	// lineaddr = tag+index, effectively a "cache block id"
-	// To interleave, map 0 -> bank 0, 1 -> bank 1, n -> lineaddr % NUM_BANKS
-	// uint8_t bank_id = lineaddr % DRAM_BANKS;
-	uint bank_id = lineaddr & (DRAM_BANKS - 1);
+	// Need to simulate row buffer hit/miss
 
-	// uint64_t row_id = lineaddr / DRAM_BANKS;
-	uint64_t row_id = lineaddr;
+	// [   ROW_ID 47b   ][HIGH_COL 7b][BANK 4bit][LOW_COL 3b][byte 3b]
+	// |---------------"lineaddr"---------------||-- cache block 6b -|
+	// |----"row id"----|
+
+	// To interleave, bank id is bottom 4 bits
+	uint bank_id = lineaddr & (DRAM_BANKS - 1); // lineaddr % DRAM_BANKS
+
+	// Division works because DRAM_BANKS and ROW_BUFFER_SIZE assumed to be powers-of-2
+	uint64_t row_id = lineaddr / DRAM_BANKS / (ROW_BUFFER_SIZE / 8); // Divide by 8 because low 3 column bits not present
 
 	uint delay = 0;
 	Row_Buffer &buf = dram->banks[bank_id];
@@ -92,16 +94,17 @@ uint64_t dram_access_mode_CDE(Dram *dram, Addr lineaddr, bool is_dram_write) {
 	if ( buf.valid && buf.row_id == row_id ) { // Row buffer hit
 		delay = LATENCY_CAS;
 	} else if ( buf.valid && buf.row_id != row_id ) { // Row buffer miss (conflict)
+		dram->stat_buffer_miss++;
 		// Fill this row
 		// buf.valid = true;
 		buf.row_id = row_id;
 		delay = LATENCY_PRE + LATENCY_RAS + LATENCY_CAS;
 	} else { // Row buffer is empty (invalid)
+		dram->stat_buffer_miss++;
 		buf.valid = true;
 		buf.row_id = row_id;
-		delay = LATENCY_PRE +  LATENCY_RAS + LATENCY_CAS;
+		delay = LATENCY_RAS + LATENCY_CAS;
 	}
 
-	// Figure out which page this lineaddr belongs to.
 	return delay;
 }
